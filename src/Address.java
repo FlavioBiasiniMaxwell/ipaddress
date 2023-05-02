8ee915cd2a409183150514bdd744921e779d065d
flavio.biasini <FLAVIO.BIASINI>
2023-05-02 09:43:26 +0200

    class address

---
+++ src/Address.java
@@ -0,0 +1,10 @@
+public class Address {
+    String DecimalDottedQuads;
+    String DottedBinary;
+    public Address(String DecimalDottedQuads){
+        this.DecimalDottedQuads =  DecimalDottedQuads;
+    }
+    private void DecimalToBinary(){
+
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
+class Main {
     public static void main(String[] args) {
+        Address ip = new Address("192.168.0.1");
+        System.out.println("IP1: " + ip.getDecimalDottedQuads());
+        System.out.println("IP1 in binario: " + ip.getDottedBinary());
+        System.out.println();
 
+        ip = new Address(-1062731775);
+        System.out.println("IP2: " + ip.getDecimalDottedQuads());
+        System.out.println("IP2 in binario: " + ip.getDottedBinary());
     }
 }
