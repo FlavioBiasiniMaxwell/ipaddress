7a179cf83c4ecdc709f6d63a7e7c573df4f15f96
flavio.biasini <FLAVIO.BIASINI>
2023-05-16 08:48:05 +0200

    almost done

--- src/Address.java
+++ src/Address.java
@@ -1,10 +1,28 @@
 public class Address {
-    String DecimalDottedQuads;
-    String DottedBinary;
-    public Address(String DecimalDottedQuads){
-        this.DecimalDottedQuads =  DecimalDottedQuads;
-    }
-    private void DecimalToBinary(){
+    private final String decimalDottedQuad;
+    private final String[] binary;
 
+    private final String DottedBinary;
+
+    //private final long decimal;
+    public Address(String decimalDottedQuad) {
+        this.decimalDottedQuad = decimalDottedQuad;
+        this.binary = decimalDottedQuad.split("//.",4);
+        this.DottedBinary = binary[0]+"."+binary[1]+"."+binary[2]+"."+binary[3];
     }
+    /*public Address(long decimal) {
+        this.decimal = decimal;
+        this.decimalDottedQuad = decimalToDottedQuad(decimal);
+        this.binary = decimalToBinary(decimalDottedQuad);
+    }
+    */
+    public String getDecimalDottedQuad() {
+        return decimalDottedQuad;
+    }
+    public String getDottedBinary(){
+        return DottedBinary;
+    }
+
+
 }
+
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 class Main {
     public static void main(String[] args) {
         Address ip = new Address("192.168.0.1");
-        System.out.println("IP1: " + ip.getDecimalDottedQuads());
+        System.out.println("IP1: " + ip.getDecimalDottedQuad());
         System.out.println("IP1 in binario: " + ip.getDottedBinary());
         System.out.println();
 
-        ip = new Address(-1062731775);
-        System.out.println("IP2: " + ip.getDecimalDottedQuads());
-        System.out.println("IP2 in binario: " + ip.getDottedBinary());
+        /*ip = new Address(-1062731775);
+        System.out.println("IP2: " + ip.getDecimalDottedQuad());
+        System.out.println("IP2 in binario: " + ip.getDottedBinary());*/
     }
 }
